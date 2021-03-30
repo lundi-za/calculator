3917ca52119f569cd22476880a9f392f3ac0dad9
Lundi Mkhungwane <216039746@mycput.ac.za>
2021-03-30 03:56:12 +0200

    Modified Addition,java file to add: doubleSum()

--- src/main/java/za/ac/mycput/calculator/Addition.java
+++ src/main/java/za/ac/mycput/calculator/Addition.java
@@ -10,7 +10,24 @@ package za.ac.mycput.calculator;
  * @author Lundi
  */
 public class Addition {
+    /**
+     * This adds two integers
+     * @param a
+     * @param b
+     * @return 
+     */
     public static int integerSum(int a, int b){
         return a + b;
     }
+    /**
+     * This adds two real numbers
+     * @param a
+     * @param b
+     * @return 
+     */
+    
+    public static double doubleSum(double a, double b) {
+        return a + b;
+    }
+    
 }
